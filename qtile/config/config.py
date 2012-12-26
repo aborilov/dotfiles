b1b937d05cec5c9a8a1ad166c97df69495304433
Aborilov Pavel <aborilov@gmail.com>
2012-12-26 14:28:03 +0400

    add two screens

--- qtile/config/config.py
+++ qtile/config/config.py
@@ -6,12 +6,13 @@ from libqtile import layout, bar, widget, hook
 modkey = "mod4"
 keys = [
     Key([modkey], "k", lazy.layout.down()),
+    Key([modkey, "shift"], "n", lazy.to_next_screen()),
     Key([modkey], "j", lazy.layout.up()),
     Key([modkey], "h", lazy.layout.grow()),
     Key([modkey], "l", lazy.layout.shrink()),
     Key([modkey], "n", lazy.layout.normalize()),
     Key([modkey], "m", lazy.layout.maximize()),
-    Key([modkey], "q", lazy.shutdown()),
+    Key([modkey, "shift"], "q", lazy.shutdown()),
     Key([modkey], "e", lazy.spawn("pcmanfm")),
     Key([modkey], "b", lazy.spawn("chromium")),
     Key([modkey], "x", lazy.spawn("xscreensaver-command --lock")),
@@ -21,6 +22,7 @@ keys = [
     Key([modkey], "Right", lazy.nextgroup()),
     Key([modkey], "t", lazy.window.disable_floating()),
     Key([modkey], "Return", lazy.spawn("urxvt -pe tabbed")),
+    Key([modkey], "F10", lazy.spawn("import -window root ~/screenshot.png")),
     Key(
         [], "XF86AudioRaiseVolume",
         lazy.spawn("amixer -c 0 -q set Master 2dB+")
@@ -46,10 +48,6 @@ keys = [
         [modkey, "shift"], "space",
         lazy.layout.flip()
     ),
-    Key(
-        [modkey, "shift"], "Return",
-        lazy.layout.toggle_split()
-    ),
     Key([modkey, "control"], "r", lazy.restart()),
     Key([modkey, "shift"], "t", lazy.window.enable_floating()),
 ]
@@ -65,8 +63,9 @@ mouse = [
 ]
 
 border = dict(
-    border_normal='#808080',
-    border_width=2,)
+    border_focus = '#ffffff',
+    border_width=2,
+    border_normal="#222222",)
 
 groups = [
     Group("Browser"),
@@ -84,7 +83,8 @@ for index, grp in enumerate(groups):
     keys.extend([
 
         # switch to group
-        Key([modkey], str(index + 1), lazy.group[grp.name].toscreen()),
+        Key([modkey], str(index + 1), lazy.group[grp.name].toscreen(0)),
+        Key([modkey, "control"], str(index + 1), lazy.group[grp.name].toscreen(1)),
 
         # send to group
         Key([modkey, "shift"], str(index + 1), lazy.window.togroup(grp.name)),
@@ -92,7 +92,7 @@ for index, grp in enumerate(groups):
     ])
 
 layouts = [
-    layout.MonadTall(),
+    layout.MonadTall(**border),
     layout.TreeTab(),
     layout.Stack(stacks=1)
 ]
@@ -121,6 +121,8 @@ screens = [
             widget.Sep(),
             widget.Clock('%H:%M:%S %d.%m.%Y',**widget_defaults),
             widget.Sep(),
+            widget.Pacman(**widget_defaults),
+            widget.Sep(),
             widget.Battery(energy_now_file = "charge_now",
                                 energy_full_file = "charge_full",
                                 power_now_file = "current_now",
@@ -128,12 +130,13 @@ screens = [
                                 charge_char = '>',
                                 discharge_char = '<',**widget_defaults)
             ],
-            24,))]
+            24,)),
+    Screen()]
 
 
 @hook.subscribe.client_new
 def grouper(window, windows={
-        'google-chrome': 'Browser',
+        'chromium': 'Browser',
         'sublime_text': 'Develope',
         'urxvt': ['Jabber', 'Terminals'],
         'pcmanfm': 'FileManager'}):
@@ -159,36 +162,62 @@ def grouper(window, windows={
 
     Window name can be found via window.window.get_wm_class()
     """
+    try:
+        windowtype = window.window.get_wm_class()[0]
 
-    windowtype = window.window.get_wm_class()[0]
+        # if the window is in our map
+        if windowtype in windows.keys():
 
-    # if the window is in our map
-    if windowtype in windows.keys():
+            # opening terminal applications gives
+            # the window title the same name, this
+            # means that we need to treat these apps
+            # differently
 
-        # opening terminal applications gives
-        # the window title the same name, this
-        # means that we need to treat these apps
-        # differently
+            if windowtype != 'urxvt':
+                window.togroup(windows[windowtype])
+                windows.pop(windowtype)
 
-        if windowtype != 'urxvt':
-            window.togroup(windows[windowtype])
-            windows.pop(windowtype)
+                # if it's not on our special list,
+                # we send it to the group and pop
+                # that entry out the map
+            else:
+                try:
+                    window.togroup(windows[windowtype][0])
+                    windows[windowtype].pop(0)
+                except IndexError:
+                    pass
+    except:
+        pass
 
-            # if it's not on our special list,
-            # we send it to the group and pop
-            # that entry out the map
-        else:
-            try:
-                window.togroup(windows[windowtype][0])
-                windows[windowtype].pop(0)
-            except IndexError:
-                pass
+floating_layout = layout.Floating(
 
+    auto_float_types=[
+        'utility',
+
+        'notification',
+        'toolbar',
+
+        'splash',
+        'dialog',
+
+    ],
+    float_rules=[
+        {'wmclass': 'sun-awt-X11-XDialogPeer'}, #vue
+
+        {'wmclass': 'sun-awt-X11-XWindowPeer'}, #vue
+
+    ],
+)
 
 @hook.subscribe.client_new
 def dialogs(window):
     if (window.window.get_wm_type() == 'dialog'
             or window.window.get_wm_transient_for()):
+        window.floating = True
+
+@hook.subscribe.client_new
+def vue_tools(window):
+    if window.window.get_wm_class() == ('sun-awt-X11-XDialogPeer', 'java-lang-Thread'):
         window.floating = True
 
 main = None
